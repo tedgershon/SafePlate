d81e5bec9fda85a9717cfe176adb8cf575337069
cozey7 <clarencc@andrew.cmu.edu>
2025-11-08 16:35:18 -0500

    2nd model verifies first, keys are in .env.

--- safeplate_project/settings.py
+++ safeplate_project/settings.py
@@ -3,10 +3,14 @@ Django settings for safeplate_project project.
 """
 
 from pathlib import Path
-import os # Import os
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
